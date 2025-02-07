4857bac664c20e3feb7abec3bf004c5172e67c28
PD <89239979+PDgaming@users.noreply.github.com>
2025-02-07 20:39:42 +0530

    added route protection

---
+++ src/allowedURLs.ts
@@ -0,0 +1,4 @@
+export const allowedUrls: string[] = [
+    "http://localhost:5173",
+    "https://pd-enterprise.pages.dev"
+]
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,80 @@
-import { Hono } from 'hono'
-import { db } from './db'
-import { posts } from './db/schema'
-import { cors } from "hono/cors"
-import { eq } from "drizzle-orm"
+import { Hono } from 'hono' // Import Hono framework for building web applications
+import { db } from './db' // Import database connection
+import { posts } from './db/schema' // Import posts schema from database
+import { cors } from "hono/cors" // Import CORS middleware for handling cross-origin requests
+import { eq } from "drizzle-orm" // Import equality function for query building
+import { allowedUrls } from './allowedURLs' // Import list of allowed URLs for CORS
 
+// Create a new Hono application instance
 const app = new Hono()
+function validateRoute(url: string) {
+    if (allowedUrls.includes(url)) {
+        return true
+    } else {
 
+        return false
+    }
+}
+
+
+// Define a route for the root URL
 app.get('/', (c) => {
+    c.status(200)
     return c.text("This is the backend-service for PD Enterprise.")
 })
+// Define a route for handling not found errors
 app.notFound((c) => {
+    c.status(404) // Set the HTTP status code to 404 (Not Found)
     return c.json({
-        status: 404,
-        message: "Not Found",
-    })
+        status: 404, // Status code
+        message: "Not Found", // Error message
+    }) // Return a JSON response
 })
+// Define a route for handling errors
 app.onError((err, c) => {
+    c.status(500) // Set the HTTP status code to 500 (Internal Server Error)
     return c.json({
-        status: 500,
-        message: "Internal Server Error",
-        error: err,
-    })
+        status: 500, // Status code
+        message: "Internal Server Error", // Error message
+        error: err, // The error object
+    }) // Return a JSON response
 })
+// Apply CORS middleware to the /pd-enterprise/* routes
 app.use("/pd-enterprise/*", cors())
-app.get("/pd-enterprise/blog/posts", async (c) => {
-    try {
-        const allPosts = await db.select().from(posts);
-        return c.json({ status: 200, data: allPosts })
-    } catch (error) {
-        return c.json({ error })
-    }
-})
-app.get("/pd-enterprise/blog/posts/:slug", async (c) => {
-    const slug = c.req.param("slug");
-    try {
-        const post = await db.select().from(posts).where(eq(posts.slug, slug))
-        return c.json({ status: 200, data: post })
-    } catch (error) {
-        return c.json({ error })
-    }
-})
-// HTTP Methods
-app.get('/', (c) => c.text('GET /'))
-app.post('/', (c) => c.text('POST /'))
-app.put('/', (c) => c.text('PUT /'))
-app.delete('/', (c) => c.text('DELETE /'))
 
+// Define a route for fetching all blog posts
+app.get("/pd-enterprise/blog/posts", async (c) => {
+    if (validateRoute(c.req.header("origin") || '')) {
+        try {
+            const allPosts = await db.select().from(posts); // Fetch all posts from the database
+            c.status(200); // Set the HTTP status code to 200 (OK)
+            return c.json({ status: 200, data: allPosts }) // Return a JSON response with the posts data
+        } catch (error) {
+            c.status(500); // Set the HTTP status code to 500 (Internal Server Error)
+            return c.json({ error }) // Return a JSON response with the error
+        }
+    } else {
+        c.status(500)
+        return c.json({ status: 500, message: "Origin not allowed", allowedUrls })
+    }
+})
+// Define a route for fetching a single blog post by slug
+app.get("/pd-enterprise/blog/posts/:slug", async (c) => {
+    if (validateRoute(c.req.header("origin") || "")) {
+        const slug = c.req.param("slug");
+        try {
+            const post = await db.select().from(posts).where(eq(posts.slug, slug))
+            c.status(200);
+            return c.json({ status: 200, data: post })
+        } catch (error) {
+            c.status(500);
+            return c.json({ error })
+        }
+    } else {
+        c.status(500)
+        return c.json({ status: 500, message: "Origin not allowed", allowedUrls })
+    }
+})
+
+// Export the application instance
 export default app
