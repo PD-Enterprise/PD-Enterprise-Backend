d54de3d966961bcff3b5f18a1c4b07607982a372
PD <89239979+PDgaming@users.noreply.github.com>
2025-02-15 19:09:46 +0530

    added cnotes url

--- src/allowedURLs.ts
+++ src/allowedURLs.ts
@@ -1,5 +1,6 @@
 export const allowedUrls: string[] = [
     "http://localhost:5173",
     "http://localhost:4173",
-    "https://pd-enterprise.pages.dev"
+    "https://pd-enterprise.pages.dev",
+    "https://cnotes.pages.dev",
 ]
