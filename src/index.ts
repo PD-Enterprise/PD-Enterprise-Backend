ff592b028833991c54f769bbd8d7a11b6ed32d39
PD <89239979+PDgaming@users.noreply.github.com>
2025-03-10 16:47:42 +0530

    implemented separate prompt system for different modal results

--- src/index.ts
+++ src/index.ts
@@ -18,20 +18,32 @@ dotenv.config() // Load environment variables from .env file
 // Create a new Hono application instance
 const app = new Hono()
 let chatHistory: any = []
-const initial_message = {
+const initial_socratic_message = {
     "role": "user",
     "content": `You are a Socratic Teacher. And I am your student. And please try to keep your replies as brief as possible, while explaining each topic carefully. Please Explain the topic in relation to the NCERT CBSE 2024 curriculum. Please don't give the answer directly but rather a starting point to get started, your job is to help the student find the answer in his/her own way.
 Also please return your answer in HTML format, with proper tags.`,
 }
-chatHistory = [...chatHistory, initial_message]
+const initial_message = {
+    "role": "user",
+    "content": `Please return your answer in HTML format, with proper tags, only send inside the <body> tags, no need for the boilerplate or <body> tag.
+        always return your whole answer strictly in the following json syntax: {summary: "[a short summary in a few words of the prompt"]", content: "[your response]"}`,
+}
+let chatCompletion: any
 
 // FUNCTIONS
-async function chatWithHistory(userMessage: string, modal: string, groq: any) {
+async function chatWithHistory(userMessage: string, modal: string, groq: any, modalParams: any) {
     chatHistory = [...chatHistory, { role: "user", content: userMessage }]
-    const chatCompletion = await groq.chat.completions.create({
-        messages: chatHistory,
-        model: modal
-    })
+    if (modalParams.type == "custom") {
+        chatCompletion = await groq.chat.completions.create({
+            messages: [...chatHistory, initial_socratic_message],
+            model: modal
+        })
+    } else if (modalParams.type == "direct") {
+        chatCompletion = await groq.chat.completions.create({
+            messages: [...chatHistory, initial_message],
+            model: modal
+        })
+    }
     const response = chatCompletion.choices[0]?.message?.content
     chatHistory = [...chatHistory, { role: "assistant", content: response }]
     return response
@@ -133,7 +145,6 @@ app.post("/users/roles/get-role", async (c) => {
     }
 })
 
-
 // CNOTES API ROUTES
 app.post("/notes/notes", async (c) => {
     if (validateRoute(c.req.header("origin") || "")) {
@@ -194,13 +205,14 @@ app.post("/ai/chat/:modal", async (c) => {
 
         const modal = c.req.param("modal")
         const body = await c.req.json()
-        if (!body.prompt) {
+        const modalParams = body.modalParams
+        if (!body.prompt || !body.modalParams) {
             c.status(400)
             return c.json({ status: 400, message: "Missing required fields", data: null, error: null })
         }
         const prompt = body.prompt
         try {
-            const chatCompletion = await chatWithHistory(prompt, modal, groq)
+            const chatCompletion = await chatWithHistory(prompt, modal, groq, modalParams)
             return c.json({ status: 200, message: "Successfully found note", data: chatCompletion, error: null })
         } catch (error) {
             console.error(error)
