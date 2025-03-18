e0d733d94ef60c0c6030a7cce9d6327583bc8871
PD <89239979+PDgaming@users.noreply.github.com>
2025-03-18 12:43:31 +0530

    added dev branch in allowed URLs list

--- src/allowedURLs.ts
+++ src/allowedURLs.ts
@@ -3,4 +3,5 @@ export const allowedUrls: string[] = [
     "http://localhost:4173",
     "https://pd-enterprise.pages.dev",
     "https://cnotes.pages.dev",
+    "https://dev.pd-enterprise.pages.dev"
 ]
