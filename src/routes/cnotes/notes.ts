6c3d0f982091381e9738ed7d831f0c896d2eb506
PDgaming <produnyadehingia12345@gmail.com>
2025-11-15 18:11:36 +0530

    updated code in notes endpoint

--- src/routes/cnotes/notes.ts
+++ src/routes/cnotes/notes.ts
@@ -1,65 +1,59 @@
 import { Hono } from "hono";
 import { eq } from "drizzle-orm";
 import { notesdb } from "../../db/cnotes";
-import { notes, academicLevel } from "../../../drizzle/cnotes/schema";
+import { notes, academicLevel, user } from "../../../drizzle/cnotes/schema";
 import { userExistsInNotesDb } from "../../utils/userExistsInNoteDb";
+import { returnJson } from "../../utils/returnJson";
 
 const notesRouter = new Hono();
 
 notesRouter.post("/notes", async (c) => {
   const body = await c.req.json();
-  if (!body.email) {
-    c.status(400);
-    return c.json({
-      status: 400,
-      message: "Missing required fields",
-      data: null,
-      error: null,
-    });
-  }
   const email = body.email;
-  // Auth check
-  if (!(await userExistsInNotesDb(email))) {
-    c.status(401);
-    return c.json({
-      status: 401,
-      message: "Unauthorized: Email does not exist in notes database.",
-      data: null,
-      error: null,
-    });
+  if (!email) {
+    c.status(400);
+    return c.json(returnJson(400, "Missing required fields", null, null));
   }
+
+  const [success, error] = await userExistsInNotesDb(email);
+  if (error || !success) {
+    return c.json(
+      returnJson(401, "Unauthorized: Email does not exist.", null, null)
+    );
+  }
+
   try {
-    const getNotes = await notesdb
+    const notesList = await notesdb
       .select({
         title: notes.title,
         slug: notes.slug,
         content: notes.content,
         dateCreated: notes.dateCreated,
-        academicLevel: academicLevel.academicLevel,
-        grade: academicLevel.grade,
-        subject: notes.topic,
+        topic: notes.topic,
         type: notes.type,
+        visibility: notes.visibility,
+        academicLevel: academicLevel.academicLevel,
+        year: notes.year,
+        language: notes.language,
+        keywords: notes.keywords,
       })
       .from(notes)
       .innerJoin(academicLevel, eq(notes.academicLevel, academicLevel.id))
-      .innerJoin(noteUser, eq(notes.email, noteUser.id))
-      .where(eq(noteUser.email, email));
-    // console.log(getNotes);
-    return c.json({
-      status: 200,
-      message: "Notes found successfully",
-      data: getNotes,
-      error: null,
-    });
+      .innerJoin(user, eq(notes.email, user.id))
+      .where(eq(user.email, email));
+
+    return c.json(returnJson(200, "Notes found successfully", notesList, null));
   } catch (error) {
     console.error(error);
     c.status(500);
-    return c.json({
-      status: 500,
-      message: "An unexpected error occurred. Please try again later.",
-      data: null,
-      error: null,
-    });
+    return c.json(
+      returnJson(
+        500,
+        "An unexpected error occurred. Please try again later.",
+        null,
+        null
+      )
+    );
   }
 });
 
