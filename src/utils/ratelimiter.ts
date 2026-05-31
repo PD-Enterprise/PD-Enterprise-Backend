62b88a0c68c312216105607a770969350e880d41
PDgaming <produnyadehingia12345@gmail.com>
2026-05-31 12:49:19 +0530

    feat: added a directory for middlewares that incluedes a middle ware for authenticating users and a ratelimiter

--- src/utils/ratelimiter.ts
+++
@@ -1,9 +0,0 @@
-import rateLimit from "hono-rate-limit";
-
-export function rateLimiter(limit: number = 20) {
-  return rateLimit({
-    windowMs: 60 * 1000, // 60,000 ms = 1 minute
-    limit: limit, // 20 requests per minute (≈ 0.33 requests/sec)
-    message: "Too many requests, please try again later.",
-  });
-}
