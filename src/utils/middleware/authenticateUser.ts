800f4c90ce79b3258890dffb3b93fa1bad1ba5cb
PDgaming <produnyadehingia12345@gmail.com>
2026-06-17 21:42:29 +0530

    bug fix: added authjs session token name in cookie check

--- src/utils/middleware/authenticateUser.ts
+++ src/utils/middleware/authenticateUser.ts
@@ -5,7 +5,7 @@ import { decode } from "@auth/core/jwt";
 import { AppVariables, Bindings, userObject } from "@/src/types";
 
 export const authUser: MiddlewareHandler<{ Bindings: Bindings, Variables: AppVariables }> = async (c, next) => {
-    const sessionToken = getCookie(c, "authjs.session-token") || getCookie(c, "__Secure-better-auth.session_token")
+    const sessionToken = getCookie(c, "authjs.session-token") || getCookie(c, "__Secure-authjs.session-token")
     if (!sessionToken) {
         c.status(401)
         return c.json(returnJson(401, "Unauthorized: No session token found", null, null), 401)
@@ -14,9 +14,7 @@ export const authUser: MiddlewareHandler<{ Bindings: Bindings, Variables: AppVar
         const decoded = await decode({
             token: sessionToken,
             secret: c.env.AUTH_SECRET!,
-            salt: sessionToken.startsWith("__Secure")
-                ? "__Secure-better-auth.session_token"
-                : "authjs.session-token",
+            salt: "authjs.session-token",
         })
         // console.log(decoded)
         if (!decoded?.email) {
